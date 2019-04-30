041edf125c9531518774b7884a7df482b0d026bf
Xiaodong Sun <xiaodong.sun@61topbaby.com>
2019-04-30 10:16:38 +0800

    房源列表查询、图片显示

--- src/pages/haoke/House/Resource.js
+++ src/pages/haoke/House/Resource.js
@@ -10,12 +10,13 @@ import {
   Select,
   Icon,
   Button,
-  Divider,
+  Divider, Carousel,
 } from 'antd';
 import StandardTable from '@/components/StandardTable';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
 import styles from '../TableList.less';
+import ShowPics from "./ShowPics";
 
 const FormItem = Form.Item;
 const { Option } = Select;
@@ -23,6 +24,14 @@ const getValue = obj =>
   Object.keys(obj)
     .map(key => obj[key])
     .join(',');
+
+const payType = new Map([
+  [1,'付一押一'],
+  [2,'付三押一'],
+  [3,'付六押一'],
+  [4,'年付押一'],
+  [5,'其它'],
+]);
 
 /* eslint react/no-multi-comp:0 */
 @connect(({ houseResource, loading }) => ({
@@ -43,44 +52,55 @@ class Resource extends PureComponent {
   columns = [
     {
       title: '房源编号',
-      dataIndex: 'name',
+      dataIndex: 'id',
     },
     {
       title: '房源信息',
-      dataIndex: 'desc',
+      dataIndex: 'title',
     },
     {
       title: '图',
-      dataIndex: 'pic'
+      dataIndex: 'pic',
+      render : (text, record, index) => {
+        return  <ShowPics pics={text}/>
+      }
     },
     {
-      title: '委托人',
-      dataIndex: 'status'
+      title: '楼栋',
+      render : (text, record, index) => {
+        return record.buildingFloorNum + "栋"+record.buildingNum+"单元"+record.buildingUnit+"号"
+      }
+
     },
     {
-      title: '委托时间',
-      dataIndex: 'updatedAt',
-      render: val => <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>,
+      title: '支付方式',
+      render : (text, record, index) => {
+        return payType.get(record.paymentMethod)
+      }
     },
     {
-      title: '咨询量',
-      dataIndex: 'status'
+      title: '户型',
+      dataIndex: 'houseType'
+
     },
     {
-      title: '看房量',
-      dataIndex: 'status'
+      title: '面积',
+      dataIndex: 'useArea',
+      render : (text, record, index) => {
+        return text+"平方"
+      }
     },
     {
-      title: '状态',
-      dataIndex: 'status'
+      title: '楼层',
+      dataIndex: 'floor'
     },
     {
       title: '操作',
       render: (text, record) => (
         <Fragment>
-          <a onClick={() => this.handleUpdateModalVisible(true, record)}>查看详情</a>
+          <a onClick={() => this.handleUpdateModalVisible(true, record)}>查看</a>
           <Divider type="vertical" />
-          <a href="">订阅警报</a>
+          <a href="">删除</a>
         </Fragment>
       ),
     },
--- src/services/houseResource.js
+++ src/services/houseResource.js
@@ -2,5 +2,5 @@ import request from '@/utils/request';
 import { stringify } from 'qs';
 
 export async function queryResource(params) {
-  return request(`/haoke/resource?${stringify(params)}`);
+  return request(`/haoke/house/resources?${stringify(params)}`);
 }
